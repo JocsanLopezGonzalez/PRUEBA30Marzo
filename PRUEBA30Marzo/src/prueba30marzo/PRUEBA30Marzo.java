6e6ef952bbf68f8057547401503a55a14f92a16b
Jocsan Lopez <yocsanyl77@gmail.com>
2020-04-02 13:49:13 -0600

    Prueba

---
+++ PRUEBA30Marzo/src/prueba30marzo/PRUEBA30Marzo.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba30marzo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class PRUEBA30Marzo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Jocsan Abisaí López González");
+        System.out.println("Zona 11, Huehuetenango, Huehuetenango");
+        System.out.println("56158681");
+    }
+    
+}
