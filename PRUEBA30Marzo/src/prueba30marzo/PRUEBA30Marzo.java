adce70220de69f79a9be8c3a65657797ee6de138
Jocsan Lopez <yocsanyl77@gmail.com>
2020-04-02 18:29:23 -0600

    Update PRUEBA30Marzo.java

--- PRUEBA30Marzo/src/prueba30marzo/PRUEBA30Marzo.java
+++ PRUEBA30Marzo/src/prueba30marzo/PRUEBA30Marzo.java
@@ -18,22 +18,29 @@ public class PRUEBA30Marzo {
         System.out.println("Jocsan Abisaí López González");
         System.out.println("Zona 11, Huehuetenango, Huehuetenango");
         System.out.println("56158681");
+        System.out.println();
         
         System.out.println("Denis Joel Cifuentes Cano");
         System.out.println("Zona 1, Chiantla");
         System.out.println("45883960");
+        System.out.println();
         
         System.out.println("Carlos Andrés Miranda Méndez");
         System.out.println("1a Calle A, Colonia Villas del Magisterio Zona 9");
         System.out.println("58385781");
+        System.out.println();
         
         System.out.println("JAN CARLO DE LEÓN CIFUENTES");
         System.out.println("11 CALLE 2-43 ZONA 5 COLONIA LOS ENCINOS");
         System.out.println("33361433");
-        
+        System.out.println();
+                
         System.out.println("Nombre: Luis Humberto Ruiz Castillo");
         System.out.println("Direccion: 8va Calle A 11-30 zona 1 interior");
         System.out.println("Telefono: 37070944");
+        System.out.println();   
+       
     }
+ 
     
 }
